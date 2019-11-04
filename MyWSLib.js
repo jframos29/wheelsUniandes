76f1b1a23d83ff54f3e1bc9a97ec072f1b08cd10
Juan Felipe Ramos Correa <jf.ramos@uniandes.edu.co>
2019-11-04 03:32:04 -0500

    Reactividad al cien

--- MyWSLib.js
+++ MyWSLib.js
@@ -2,21 +2,31 @@ const WebSocket = require("ws");
 
 const MyWSLib = function() {
   const MyWSLib = this || {};
-  const clients = [];
+  const clients = {};
 
   MyWSLib.setupWS = server => {
-    const wss = new WebSocket.Server({ server });
 
+    const wss = new WebSocket.Server({ server });
     console.log("setting up socket");
+
     wss.on("connection", ws => {
       console.log("Accepting connection");
-      clients.push(ws);
+      ws.on("message", message =>{
+        clients[message]=ws;
+        console.log("Connection accepted", message);
+      });
     });
   };
 
   MyWSLib.notifyAll = data => {
     for (let ws of clients) {
       ws.send(data);
+    }
+  };
+
+  MyWSLib.notifyChangeCar = data =>{
+    for(let ws of clients){
+
     }
   };
 
