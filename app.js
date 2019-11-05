bb9c4d2a316c9b062d852788e509d4567d55304e
Juan Felipe Ramos Correa <jf.ramos@uniandes.edu.co>
2019-11-04 22:07:24 -0500

    Cambios varios

--- app.js
+++ app.js
@@ -1,51 +1,28 @@
-var createError = require("http-errors");
 var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var bodyParser = require("body-parser");
 var logger = require("morgan");
+var cors = require("cors");
 
 var indexRouter = require("./routes/index");
 var authRouter = require("./routes/auth");
 var carsRouter = require("./routes/cars");
 var serviceRouter = require("./routes/services");
-var cors = require("cors");
+
+
 var app = express();
 
-// view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "jade");
-
-app.use(logger("dev"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "front/build")));
-app.get("*", (req,res)=>{
-  res.sendFile(path.join(path.join(__dirname,"front/build"),"index.html"));
-});
+app.use(logger("dev"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
 app.use(cors());
+
 app.use("/", indexRouter);
 app.use("/auth",authRouter);
 app.use("/cars", carsRouter);
 app.use("/services", serviceRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render("error");
-});
 
 module.exports = app;
