562a7be6c37ffbd69c39db42b8e9d2ab2cdba13b
Juan Felipe Ramos Correa <jf.ramos@uniandes.edu.co>
2019-11-04 21:49:04 -0500

    Ahora sí

---
+++ front/build/precache-manifest.a534c4bc2a75038fd02147642711d8ab.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8348a15d15d8cd2a9122bd1dfabb2005",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a332e4ac2deaa2a26894",
+    "url": "/static/css/2.833dd627.chunk.css"
+  },
+  {
+    "revision": "739e0fcbd47e71fc3f41",
+    "url": "/static/css/main.f81babcd.chunk.css"
+  },
+  {
+    "revision": "a332e4ac2deaa2a26894",
+    "url": "/static/js/2.2c0b716a.chunk.js"
+  },
+  {
+    "revision": "739e0fcbd47e71fc3f41",
+    "url": "/static/js/main.9fc57bd4.chunk.js"
+  },
+  {
+    "revision": "ebd3a683af0fa2dbb56f",
+    "url": "/static/js/runtime-main.7139b951.js"
+  },
+  {
+    "revision": "62aedac87bcf45634c808da8443792c4",
+    "url": "/static/media/crear.62aedac8.png"
+  },
+  {
+    "revision": "5f58e95ed2765cc5b1d2dd6f40f62e65",
+    "url": "/static/media/logo.5f58e95e.png"
+  },
+  {
+    "revision": "d5049a155aab3ec4778a3cb01f9a725d",
+    "url": "/static/media/logo2.d5049a15.png"
+  },
+  {
+    "revision": "be6809a150fdcf519bb363d182d99b60",
+    "url": "/static/media/unirse.be6809a1.png"
+  }
+]);
